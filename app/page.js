a17a7a8e7217dd5da85b2ede4fc371e736444414
HARIOM-JHA01 <jhahariom040@gmail.com>
2024-08-10 10:36:20 +0530

    Update Hero component styling and content

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <main>
             <Hero />
-            <section className="grid grid-cols-4 gap-4 p-10 bg-[#3B82F680]">
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-10 bg-[#3B82F680]">
                 {data.map((item) => (
                     <Card
                         key={item.id}
@@ -15,13 +15,13 @@ export default function Home() {
                     />
                 ))}
             </section>
-            <section className="p-10 bg-[#3B82F680] grid grid-cols-2">
-                <section className="flex flex-col py-20 leading-normal">
-                    <h3 className="text-3xl font-bold">
-                        {" "}
-                        Why choose apnasaathi?{" "}
+
+            <section className="p-6 bg-[#3B82F680] grid grid-cols-1 md:grid-cols-2 gap-6">
+                <section className="flex flex-col py-10 md:py-20 leading-normal">
+                    <h3 className="text-2xl md:text-3xl font-bold">
+                        Why choose apnasaathi?
                     </h3>
-                    <p className="pt-2 leading-loose text-md">
+                    <p className="pt-2 leading-loose text-sm md:text-md">
                         At Apna Sarthi, we are dedicated to providing a seamless
                         and enjoyable travel experience. Our platform offers a
                         wide range of vehicles, flexible rental options, and
@@ -35,7 +35,7 @@ export default function Home() {
                     <div className="grid grid-cols-2 grid-rows-2 gap-4">
                         <div className="border-2 rounded h-full p-4 bg-white">
                             <svg
-                                className="w-12 h-12"
+                                className="w-12 h-12 mx-auto"
                                 viewBox="-2.4 -2.4 28.80 28.80"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -49,25 +49,26 @@ export default function Home() {
                                     stroke-linejoin="round"
                                 ></g>
                                 <g id="SVGRepo_iconCarrier">
-                                    {" "}
                                     <path
                                         d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
                                         stroke=""
                                         stroke-width="2"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
-                                    ></path>{" "}
+                                    ></path>
                                 </g>
                             </svg>
-                            <h4>Diverse fleet</h4>
-                            <p>
+                            <h4 className="text-center mt-2 text-md font-semibold">
+                                Diverse fleet
+                            </h4>
+                            <p className="text-center text-sm">
                                 Choose from a wide selection of vehicles,
                                 including sedans, SUVs, and luxury cars.
                             </p>
                         </div>
                         <div className="border-2 rounded h-full p-4 bg-white">
                             <svg
-                                className="w-12 h-12"
+                                className="w-12 h-12 mx-auto"
                                 viewBox="-2.4 -2.4 28.80 28.80"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -81,25 +82,26 @@ export default function Home() {
                                     stroke-linejoin="round"
                                 ></g>
                                 <g id="SVGRepo_iconCarrier">
-                                    {" "}
                                     <path
                                         d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
                                         stroke=""
                                         stroke-width="2"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
-                                    ></path>{" "}
+                                    ></path>
                                 </g>
                             </svg>
-                            <h4>Diverse fleet</h4>
-                            <p>
-                                Choose from a wide selection of vehicles,
-                                including sedans, SUVs, and luxury cars.
+                            <h4 className="text-center mt-2 text-md font-semibold">
+                                Flexible Rental Options
+                            </h4>
+                            <p className="text-center text-sm">
+                                Enjoy the flexibility of choosing the rental
+                                duration that suits your travel plans.
                             </p>
                         </div>
                         <div className="border-2 rounded h-full p-4 bg-white">
                             <svg
-                                className="w-12 h-12"
+                                className="w-12 h-12 mx-auto"
                                 viewBox="-2.4 -2.4 28.80 28.80"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -113,25 +115,27 @@ export default function Home() {
                                     stroke-linejoin="round"
                                 ></g>
                                 <g id="SVGRepo_iconCarrier">
-                                    {" "}
                                     <path
                                         d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
                                         stroke=""
                                         stroke-width="2"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
-                                    ></path>{" "}
+                                    ></path>
                                 </g>
                             </svg>
-                            <h4>Diverse fleet</h4>
-                            <p>
-                                Choose from a wide selection of vehicles,
-                                including sedans, SUVs, and luxury cars.
+                            <h4 className="text-center mt-2 text-md font-semibold">
+                                Curated Tour Packages
+                            </h4>
+                            <p className="text-center text-sm">
+                                Discover Bihar with our carefully curated tour
+                                packages, designed to give you an authentic
+                                experience.
                             </p>
                         </div>
                         <div className="border-2 rounded h-full p-4 bg-white">
                             <svg
-                                className="w-12 h-12"
+                                className="w-12 h-12 mx-auto"
                                 viewBox="-2.4 -2.4 28.80 28.80"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -145,26 +149,29 @@ export default function Home() {
                                     stroke-linejoin="round"
                                 ></g>
                                 <g id="SVGRepo_iconCarrier">
-                                    {" "}
                                     <path
                                         d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
                                         stroke=""
                                         stroke-width="2"
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
-                                    ></path>{" "}
+                                    ></path>
                                 </g>
                             </svg>
-                            <h4>Diverse fleet</h4>
-                            <p>
-                                Choose from a wide selection of vehicles,
-                                including sedans, SUVs, and luxury cars.
+                            <h4 className="text-center mt-2 text-md font-semibold">
+                                Customer Support
+                            </h4>
+                            <p className="text-center text-sm">
+                                Experience 24/7 customer support to assist you
+                                with your travel needs and ensure a hassle-free
+                                journey.
                             </p>
                         </div>
                     </div>
                 </section>
             </section>
-            <section className="p-10 bg-[#3B82F680] grid grid-cols-2">
+
+            <section className="p-6 md:p-10 bg-[#3B82F680] grid grid-cols-1 md:grid-cols-2 gap-6">
                 <section className="flex items-center justify-center">
                     <Image
                         className="rounded-xl"
@@ -172,14 +179,14 @@ export default function Home() {
                         alt="Picture of Bihar"
                         width={600}
                         height={400}
+                        layout="responsive" // Ensures the image scales properly
                     />
                 </section>
-                <section className="flex flex-col py-10">
-                    <h3 className="text-3xl font-bold">
-                        {" "}
-                        Explore the beauty of Bihar{" "}
+                <section className="flex flex-col py-6 md:py-10">
+                    <h3 className="text-2xl md:text-3xl font-bold">
+                        Explore the beauty of Bihar
                     </h3>
-                    <p className="pt-2 leading-loose text-md">
+                    <p className="pt-2 leading-loose text-sm md:text-md">
                         Bihar is a state in eastern India, bordering Nepal. It
                         is divided by the River Ganges, which floods its fertile
                         plains. Important Buddhist pilgrimage sites include the
@@ -190,8 +197,8 @@ export default function Home() {
                         Gurdwara of Takht Sri Harmandir Sahib Ji.
                     </p>
 
-                    <div className="card-actions pt-6">
-                        <button className="btn btn-primary p-4">
+                    <div className="pt-4 md:pt-6">
+                        <button className="btn btn-primary p-2 md:p-4 text-sm md:text-base">
                             Book Now
                         </button>
                     </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
     return (
         <div
-            className="hero h-[60vh]"
+            className="hero sm:h-[40vh] md:h-[40vh] lg:h-[60vh] xl:h-[80vh] bg-cover bg-center bg-no-repeat"
             style={{
                 backgroundImage:
                     "url(https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
@@ -12,8 +12,10 @@ const Hero = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Apna Sarthi</h1>
-                    <p className="mb-5">
+                    <h1 className="mb-5 text-3xl md:text-5xl font-bold">
+                        Apna Sarthi
+                    </h1>
+                    <p className="mb-5 text-sm md:text-base">
                         Discover Bihar with our premium rental vehicles.
                         Self-drive or chauffeured, we offer flexible delivery
                         and pickup. Enjoy seamless tours and unforgettable
